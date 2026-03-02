2eb9303fe8979aa7e89f4a46e17b7e29286d5786
gis-14186840 <zhiwei.wu-3@student.manchester.ac.uk>
2026-03-02 19:01:57 +0000

    setup and loading data

--- GEOG71922-Assessment1.R
+++ GEOG71922-Assessment1.R
@@ -1,6 +1,22 @@
+# GEOG71922: Assessment1
+# Author [14186840]
+
+# 1.Setup and loading data
+
+#set the working directory
 setwd('E:/Manchester/S2GEOG71922_SE/Assessment1/Code/GEOG71922-Assessment1')
 
-print("hello world")
+#load the required libraries
+library(terra)
+library(sf)  #terra and sf handling spatial data
+library(dismo)  #dismo for downloading species data and evaluating models
+library(glmnet)
+library(maxnet)  #glmnet and maxnet for running Maxent models
+library(precrec)  #precrec for model evaluation
+library(dplyr)  #dplyr for data manipulation
 
+#load in the land cover map
+LCM=rast("LCMUK.tif")
 
-library(sf)
+#load in the Eurasian badger (Meles meles) data
+meles=read.csv("Melesmeles.csv")
