52fa6bdf5fded37a47a700bb62632a8532e138cf
gis-14186840 <zhiwei.wu-3@student.manchester.ac.uk>
2026-03-02 21:53:47 +0000

    add spatial cropping and projection based on Week 1

--- GEOG71922-Assessment1.R
+++ GEOG71922-Assessment1.R
@@ -25,8 +25,8 @@ meles=read.csv("Melesmeles.csv")
 #subset the data to only include points with complete coordinates
 meles<-meles[!is.na(meles$Latitude),]
 
-#subset to cases with coordinate uncertainty <100 meters
-meles<-meles[meles$Coordinate.uncertainty_m<100, ]
+#subset to cases with coordinate uncertainty less 1000 meters
+meles<-meles[meles$Coordinate.uncertainty_m <= 1000,]
 
 #Make spatial points layer
 #create crs object
@@ -34,3 +34,30 @@ meles.latlong=data.frame(x=meles$Longitude, y=meles$Latitude)
 
 #use coordinates object to create our spatial points object
 meles.sp=st_as_sf(meles.latlong, coords=c("x", "y"), crs="epsg:4326")
+
+# 2.Spatial cropping and projection
+
+#set the extent to something workable
+studyExtent<-ext(-4.2,-2.7,56.5,57.5) #list coordinates: min x, max x, min y, max y
+
+#crop points to this area
+meles.sp.crop<-st_crop(meles.sp, st_bbox(studyExtent))
+
+#set the points to the same projection as the LCM layer
+melesFin<-st_transform(meles.sp.crop,crs(LCM))
+
+#crop the land cover data to the extent of the points data (plus 5km for buffers)
+melesCoords<-st_coordinates(melesFin)
+x.min <- min(melesCoords[,1]) - 5000
+x.max <- max(melesCoords[,1]) + 5000
+y.min <- min(melesCoords[,2]) - 5000
+y.max <- max(melesCoords[,2]) + 5000
+extent.new <- ext(x.min, x.max, y.min, y.max)
+
+#crop the LCM raster to the extent
+LCM_crop <- crop(LCM$LCMUK_1, extent.new)
+
+#test drawing
+plot(LCM)
+plot(melesFin,add=TRUE)
+
