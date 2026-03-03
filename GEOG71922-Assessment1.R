44726c32f04df654b7522c6b49af7dd37ed8fb73
gis-14186840 <zhiwei.wu-3@student.manchester.ac.uk>
2026-03-03 10:40:10 +0000

    generate some background (or “pseudo-absence”) points for study area

--- GEOG71922-Assessment1.R
+++ GEOG71922-Assessment1.R
@@ -90,4 +90,52 @@ allEnv=c(wood_1800, urban_2300)
 names(allEnv)=c("broadleaf","urban")
 
 #temp check result
-print(allEnv)
+#print(allEnv)
+
+# 4.Generate background seeds and dataset
+
+#create background points
+set.seed(11)
+
+#sample background - one point for every cell (9775)
+back = spatSample(allEnv,size=2000,as.points=TRUE,method="random",na.rm=TRUE) 
+back=back[!is.na(back$broadleaf),]
+back=st_as_sf(back,crs="EPSG:27700")
+
+#get environmental covariates at presence locations
+eP=terra::extract(allEnv, vect(melesFin))
+
+#bind together the presence data
+Pres.cov=st_as_sf(cbind(eP,melesFin))
+Pres.cov$Pres=1
+
+#Remove the first column which is just an ID field.
+Pres.cov=Pres.cov[,-1]
+
+#get coordinates for spatial cross-validation later
+coordsPres=st_coordinates(Pres.cov)
+
+#drop geometry column
+Back.cov=st_as_sf(data.frame(back,Pres=0))
+
+#get coordinates of background points for cross validation later
+coordsBack=st_coordinates(back)
+
+#combine
+coords=data.frame(rbind(coordsPres,coordsBack))
+
+#assign coumn names
+colnames(coords)=c("x","y")
+
+#combine pres and background
+all.cov=rbind(Pres.cov,Back.cov)
+
+#add coordinates
+all.cov=cbind(all.cov,coords)
+
+#remove any NAs
+all.cov=na.omit(all.cov)
+all.cov=st_drop_geometry(all.cov)
+
+#test result
+print(all.cov)
